e36bd4ceda9550a6b677705435525f6e3b1fe593
Ruben Desktop <ruben.freireandrade17@gmail.com>
2024-02-04 07:51:32 +0000

    adding await

--- functions/api/stripe.js
+++ functions/api/stripe.js
@@ -2,7 +2,7 @@ import * as checkout from '../../src/checkout';
 
 export async function onRequest(context) {
     console.log("in stripe endpoint");
-    const res = checkout.create(context, context.env.STRIPE_TEST_SECRET_KEY);
+    const res = await checkout.create(context, context.env.STRIPE_TEST_SECRET_KEY);
     console.log(res);
     return res;
 }
