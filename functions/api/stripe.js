28a69cec38388831974f038b45a7de637c7cabf8
Ruben Desktop <ruben.freireandrade17@gmail.com>
2024-02-04 08:00:09 +0000

    Setting headers

--- functions/api/stripe.js
+++ functions/api/stripe.js
@@ -4,5 +4,9 @@ export async function onRequest(context) {
     console.log("in stripe endpoint");
     const res = checkout.create(context, context.env.STRIPE_TEST_SECRET_KEY);
     console.log(res);
-    return new Response(res);
+    return new Response(res, {
+        headers: {
+            'content-type': 'text/html',
+        },
+    });
 }
