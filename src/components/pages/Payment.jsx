2c3ab5b70eb69a09f7afb11c89aef0d7e35dbc5b
Ruben Desktop <ruben.freireandrade17@gmail.com>
2024-02-04 09:07:21 +0000

    api to worker.

--- src/components/pages/Payment.jsx
+++ src/components/pages/Payment.jsx
@@ -6,7 +6,7 @@ const Payment = ({onSubmit}) => {
         <div>
             <h2>Step 3: Payment</h2>
             {/* Payment details input fields go here */}
-            <form action='/api/stripe' method='POST'>
+            <form action='https://plain-queen-7268.info-majesticmiles.workers.dev/api/checkout' method='POST'>
                 <button type='submit' /*onClick={onSubmit}*/>Submit</button>
             </form>
         </div>
