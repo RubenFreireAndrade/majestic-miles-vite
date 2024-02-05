6ce39bb313fe2e94839131a6c8778992bcb56155
Ruben Desktop <ruben.freireandrade17@gmail.com>
2024-02-05 11:22:25 +0000

    Trying traditional way of importing file.

--- functions/api/stripe.js
+++ functions/api/stripe.js
@@ -1,7 +1,8 @@
-import * as checkout from '../../src/checkout';
+//import * as checkout from '../../src/checkout';
+const checkout = require('../../src/checkout');
 
 export async function onRequest(context) {
     console.log("in stripe endpoint");
-    return await checkout.create(context, context.env.STRIPE_TEST_SECRET_KEY);
+    return checkout.create(context, context.env.STRIPE_TEST_SECRET_KEY);
     //console.log(res);
 }
